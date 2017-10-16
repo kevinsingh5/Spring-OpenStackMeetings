3fe47490319715ed85f02dcf8535e3a174a91313
Kevin Singh <singhk7@yahoo.com>
2017-10-16 17:14:02 -0500

    added unit tests for Service class

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
 name: Kevin Singh
 eid: ks37843
 bitbucketid: singh5
-comments: Unit testing still missing.
+comments: Unit tests implemented.
--- src/test/java/TestOpenStackMeetingsServiceImpl.java
+++ src/test/java/TestOpenStackMeetingsServiceImpl.java
@@ -5,24 +5,52 @@
  */
 
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
-
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+
 public class TestOpenStackMeetingsServiceImpl {
 
 	OpenStackMeetingsServiceImpl osmService = null;
+	OpenStackMeetingsServiceImpl mockOsmService = mock(OpenStackMeetingsServiceImpl.class);
 	
 	@Before
 	public void setUp() {
 		osmService = new OpenStackMeetingsServiceImpl();
+	}
+	
+	@Test
+	public void testWelcome() { // test welcome call
+		String welcome = osmService.welcome();
+		assertEquals("Welcome to Open Stack meeting statistics calculation page. " + 
+        		"Please provide project and year as query parameters.", welcome);
+	}
+	
+	@Test
+	public void testTryQuery() { // test tryQuery using try-catch for Jsoup dependency
+		
+		try {
+			String link = "http://eavesdrop.openstack.org/meetings/";
+			String value = "solum";
+			Elements mockFile = mock(Elements.class);
+			Document doc = mock(Document.class);
+			
+			when(Jsoup.connect(link).get()).thenReturn(doc);
+			when(mockOsmService.tryQuery(link, value)).thenReturn(mockFile);
+			
+			Elements files = osmService.tryQuery(link, value);
+			assertEquals(files, mockFile);
+			verify(osmService).tryQuery(link, value);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
